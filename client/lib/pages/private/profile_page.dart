047a7329a71bb7303604ac6dd1a0702d3ea6b4da
daniel <gofmandon@gmail.com>
2024-02-18 08:49:49 -0500

    safe area

--- client/lib/pages/private/profile_page.dart
+++ client/lib/pages/private/profile_page.dart
@@ -67,98 +67,100 @@ class ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverToBoxAdapter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'My Account',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  ClipOval(
-                    child: Image.asset(
-                      'assets/brutus_buckeye.jpeg', // Assuming the image is stored in the assets folder
-                      width: 80, // Adjust the width as needed
-                      height: 80, // Adjust the height as needed
+    return SafeArea(
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverToBoxAdapter(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'My Account',
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Welcome, brutus_buckeye!',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(height: 20),
-                  Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            _toggleEditPreferences(_isVisible);
-                          },
-                          child: Column(
-                            children: [Text('Edit Preferences')],
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            // Implement logout functionality
-                          },
-                          child: Row(
-                            children: [Text('Logout'), Icon(Icons.arrow_forward)],
-                          ),
-                        ),
-                      ],
+                    SizedBox(height: 10),
+                    ClipOval(
+                      child: Image.asset(
+                        'assets/brutus_buckeye.jpeg', // Assuming the image is stored in the assets folder
+                        width: 80, // Adjust the width as needed
+                        height: 80, // Adjust the height as needed
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 10),
+                    Text(
+                      'Welcome, brutus_buckeye!',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 20),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              _toggleEditPreferences(_isVisible);
+                            },
+                            child: Column(
+                              children: [Text('Edit Preferences')],
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Implement logout functionality
+                            },
+                            child: Row(
+                              children: [Text('Logout'), Icon(Icons.arrow_forward)],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+              ),
             ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                Visibility(
-                  visible: _isVisible,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      controller: _searchController,
-                      onChanged: _filterList,
-                      decoration: InputDecoration(
-                        labelText: 'I am allergic to...',
-                        fillColor: Colors.white,
-                        filled: true,
-                        border: OutlineInputBorder(),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  Visibility(
+                    visible: _isVisible,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        controller: _searchController,
+                        onChanged: _filterList,
+                        decoration: InputDecoration(
+                          labelText: 'I am allergic to...',
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Visibility(
-                  visible: _isVisible,
-                  child: SizedBox(
-                    height: 100,
-                    child: ListView.builder(
-                      itemCount: _searchList.length,
-                      itemBuilder: (context, index) {
-                        return CheckboxListTile(
-                          title: Text(_searchList[index]),
-                          value: _checkedList[index],
-                          onChanged: (bool? value) {
-                            _toggleCheckbox(index);
-                          },
-                        );
-                      },
+                  Visibility(
+                    visible: _isVisible,
+                    child: SizedBox(
+                      height: 100,
+                      child: ListView.builder(
+                        itemCount: _searchList.length,
+                        itemBuilder: (context, index) {
+                          return CheckboxListTile(
+                            title: Text(_searchList[index]),
+                            value: _checkedList[index],
+                            onChanged: (bool? value) {
+                              _toggleCheckbox(index);
+                            },
+                          );
+                        },
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
